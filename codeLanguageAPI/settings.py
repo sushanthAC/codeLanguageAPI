db27b5669e9c546fa993caaf12712ac8cd9ee7ac
Sushanth <sush.chikkamath@gmail.com>
2018-08-29 15:18:25 +0530

    JWT authentication added

--- codeLanguageAPI/settings.py
+++ codeLanguageAPI/settings.py
@@ -120,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES' : ('rest_framework.permissions.IsAuthenticatedOrReadOnly',),
+    'DEFAULT_AUTHENTICATION_CLASSES' : ('rest_framework_simplejwt.authentication.JWTAuthentication',)
+}
