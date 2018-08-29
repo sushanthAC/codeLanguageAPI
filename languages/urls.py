b800fa4a7114bd3824f3fb7c25e9c8013006d7a1
Sushanth <sush.chikkamath@gmail.com>
2018-08-29 13:59:45 +0530

    model updates

--- languages/urls.py
+++ languages/urls.py
@@ -3,7 +3,9 @@ from . import views
 from rest_framework import routers	 
 
 router = routers.DefaultRouter()
-router.register('languages', views.LanguageView	)
+router.register('languages', views.LanguageView)
+router.register('paradigms', views.ParadigmView)
+router.register('programmers', views.ProgrammerView)
 
 urlpatterns = [
     path('', include(router.urls)),
--- languages/views.py
+++ languages/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Language
-from .serializers import LanguageSerializer
+from .models import Language, Paradigm, Programmer
+from .serializers import LanguageSerializer, ParadigmSerializer, ProgrammerSerializer
 
 class LanguageView(viewsets.ModelViewSet):
 	queryset = Language.objects.all()
 	serializer_class = LanguageSerializer
+
+class ParadigmView(viewsets.ModelViewSet):
+	queryset = Paradigm.objects.all()
+	serializer_class = ParadigmSerializer
+
+class ProgrammerView(viewsets.ModelViewSet):
+	queryset = Programmer.objects.all()
+	serializer_class = ProgrammerSerializer
